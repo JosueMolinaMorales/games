e2072b542c08d0f42b5d0dc9ecf3975c6ad91cad
Josue Morales <molinajosue92@hotmail.com>
2024-09-04 00:46:47 -0400

    add more movement and clean up

--- spaceship/game.py
+++ spaceship/game.py
@@ -10,29 +10,26 @@ class Game:
         pygame.display.set_caption("Space Invaders")
         self._clock = pygame.time.Clock()
         self._running = True
-
-        self.player = Ship()
-
-    def update(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self._running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    self.player.move_left()
-                if event.key == pygame.K_RIGHT:
-                    self.player.move_right()            
-
-    def draw(self):
-        self._display_surface.blit(self._background, (0, 0))
-        self.player.draw(self._display_surface)
-        pygame.display.update()
+        self._dt = 0
+        self._all_sprites = pygame.sprite.Group()
+        self.player = Ship(self._all_sprites)
 
     def run(self):
         # Run the game's main loop
         while self._running:
-            self._clock.tick(60)
-            self.update()
-            self.draw()
+            self.dt = self._clock.tick() / 1000.0
+
+            # Update
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self._running = False
+        
+            self._all_sprites.update(self.dt)
+
+            # Draw   
+            self._display_surface.blit(self._background, (0, 0))
+            self._all_sprites.draw(self._display_surface) 
+
+            pygame.display.update()
 
         pygame.quit()
--- spaceship/ship.py
+++ spaceship/ship.py
@@ -1,21 +1,29 @@
 import pygame
 from utils import WINDOW_HEIGHT, WINDOW_WIDTH
 class Ship(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-
-        
-        self.player_surface = pygame.image.load('assets/Foozle_2DS0011_Void_MainShip/Main Ship/Main Ship - Bases/PNGs/Main Ship - Base - Full health.png').convert_alpha()
-
+    def __init__(self, groups):
+        pygame.sprite.Sprite.__init__(self, groups)
+        self.image = pygame.image.load('assets/Foozle_2DS0011_Void_MainShip/Main Ship/Main Ship - Bases/PNGs/Main Ship - Base - Full health.png').convert_alpha()
         # Fetch the rectangle object that has the dimensions of the image
         # Can update the position of this object by setting the values of rect.x and rect.y
-        self.rect = self.player_surface.get_frect(center=(WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2))
+        self.rect = self.image.get_frect(center=(WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2))
     
-        self.speed = 2
-        self.direction = pygame.math.Vector2(5, -1)
+        self.speed = 100
+        self.direction = pygame.math.Vector2(0, 0)
 
-    def update(self):
-        pass
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        self.direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+        self.direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+        # normalize the direction vector so that moving diagonally isn't faster
+        self.direction = self.direction.normalize() if self.direction else self.direction
+        self.rect.center += self.direction * self.speed * dt
+
+        recent = pygame.key.get_just_pressed()
+        if recent[pygame.K_SPACE]:
+            print('Pew pew!')
+
 
     def move_left(self):
         self.rect.x -= self.speed
@@ -23,6 +31,5 @@ class Ship(pygame.sprite.Sprite):
     def move_right(self):
         self.rect.x += self.speed
         
-    def draw(self, screen):
-        self.rect.center += self.direction * self.speed
-        screen.blit(self.player_surface, self.rect)
+    def draw(self, screen, dt):
+        screen.blit(self.image, self.rect)
